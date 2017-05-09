5d4e040479a09445fdc0bb6bdeadcdb1e9007128
Alex Read <alexread730@gmail.com>
2017-05-09 15:55:03 -0600

    added initial files

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  let url = 'http://swapi.co/api/people/';
+  let charCounter = 0;
+  let charNumber = 0;
+  $('.char-info').hide();
+
+
+  $('#next').on('click', function() {
+
+    $.getJSON(url)
+      .then(function(people) {
+        console.log(people);
+        $('.char-info').hide();
+        $('.char-info').fadeIn(1000);
+
+        //array of characters from API
+        var peopleArray = people.results;
+
+        //display of character information
+        $('#name').text(peopleArray[charCounter].name);
+        $('#born').text('Born: ' + peopleArray[charCounter].birth_year);
+        $('#gender').text('Gender: ' +peopleArray[charCounter].gender);
+        $('#height').text('Height: ' +peopleArray[charCounter].height);
+        $('#homeworld').text('Homeworld: ' +peopleArray[charCounter].homeworld);
+        //add one to character counter
+        charCounter++;
+
+        //if counter reaches 10, update url for next request to the API (10 more characters). Set charCounter to 0.
+        if (charCounter == 10) {
+          url = people.next;
+          charCounter = 0;
+        }
+
+      });
+
+    });
+
+
+});
