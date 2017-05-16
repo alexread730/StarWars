bb9fa35e6457afe19740d6452a67c053f46f84b1
Alex Read <alexread730@gmail.com>
2017-05-15 20:52:22 -0600

    added char bios

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-  const API_URL = 'https://swapi.co/api/people/?page=';
+  const PEOPLE_URL = 'https://swapi.co/api/people/?page=';
   let charArray = [];
+  let charNum = 0;
 
   $('.list-group').hide();
   $('.jumbotron').hide();
@@ -11,7 +12,7 @@ $(document).ready(function() {
   function getCharacters() {
     const promises = [];
     for (let i = 1; i < 10; i++) {
-      const promise = $.getJSON(API_URL + i);
+      const promise = $.getJSON(PEOPLE_URL + i);
       promises.push(promise)
     }
     Promise
@@ -33,17 +34,24 @@ $(document).ready(function() {
     });
   }
 
-
   function makeCharacterList(character) {
+
     //append list of characters to page
-      $('.list-group').append(
-        `<button type="button" class="btn btn-info btn-block" id="char-button" data-toggle="modal" data-target=".bs-example-modal-lg">${character.name}</button>`);
+    $('.list-group').append(
+      `<button type="button" data-index=${charNum} class="btn btn-info btn-block" id="char-button" data-toggle="modal" data-target=".bs-example-modal-lg">${character.name}</button>`);
+    charNum++;
     }
 
-  $('.btn-info').click(displayCharModal);
+  $('.list-group').on('click', '.btn', function() {
+    event.preventDefault();
+    let index = $(this).attr('data-index');
 
-  function displayCharModal() {
-    console.log('hello!');
-  }
+    $('#name').text(charArray[index].name);
+    $('#born').text(charArray[index].birth_year);
+    $('#homeworld').text();
+    $('#species').text(charArray[index].species);
+    $('#films').text(charArray[index].films);
+  });
+
 
 });
